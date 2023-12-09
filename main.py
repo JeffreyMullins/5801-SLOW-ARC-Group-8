d3f5b5816cf162c04ec738c43c0517736c20fd8b
Jeff Mullins <jeffreydmullins0@gmail.com>
2023-12-09 14:26:06 -0600

    added main file

---
+++ main.py
@@ -0,0 +1,15 @@
+# NOT OFFICAL COMMENT, PLEASE EDIT LATER: this is the main file for the SLOW-ARC system
+
+def main():
+    # this is the main function that runs first when the program starts
+    print("Hi I'm the slow arc system, I'll go do things now.")
+
+    # do things and run functions
+
+    print("Finished, have a good day!")
+
+
+if __name__ == "__main__":
+    #first func that runs the main function
+    main()
+
